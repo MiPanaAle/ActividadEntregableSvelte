4657453309944f2be46111523c90eaea4a5d1bd7
AleNavarro <ale.naengram@gmail.com>
2024-12-10 13:34:49 +0100

    añadiendo boton

---
+++ src/components/Boton.svelte
@@ -0,0 +1,29 @@
+<script>
+
+</script>
+
+<main>
+    <div class="contenedor">
+        <button>EMPEZAR</button>
+        <button>REINICIAR</button>
+    </div>
+</main>
+
+<style>
+.contenedor{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+}
+button{
+    height: 50px;
+    margin: 5px;
+    padding: 20px;
+    background-color: rgb(102, 102, 102);
+    display: flex;
+    text-align: center;
+    justify-content: center;
+    align-items: center;
+}
+</style>
