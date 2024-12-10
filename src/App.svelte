31e9b9449863f11a07d3d9ec742f57a90da753ad
AleNavarro <ale.naengram@gmail.com>
2024-12-10 12:12:42 +0100

    fisrt commit

---
+++ src/App.svelte
@@ -0,0 +1,114 @@
+<script>
+
+</script>
+
+<main>
+  <div class="tablero">
+    <div class="filas">
+      <div class="casilla blanco"></div>
+      <div class="casilla negro"></div>
+      <div class="casilla blanco"></div>
+      <div class="casilla negro"></div>
+      <div class="casilla blanco"></div>
+      <div class="casilla negro"></div>
+      <div class="casilla blanco"></div>
+      <div class="casilla negro"></div>
+    </div>
+    <div class="filas">
+      <div class="casilla negro"></div>
+      <div class="casilla blanco"></div>
+      <div class="casilla negro"></div>
+      <div class="casilla blanco"></div>
+      <div class="casilla negro"></div>
+      <div class="casilla blanco"></div>
+      <div class="casilla negro"></div>
+      <div class="casilla blanco"></div>
+    </div>
+    <div class="filas">
+      <div class="casilla blanco"></div>
+      <div class="casilla negro"></div>
+      <div class="casilla blanco"></div>
+      <div class="casilla negro"></div>
+      <div class="casilla blanco"></div>
+      <div class="casilla negro"></div>
+      <div class="casilla blanco"></div>
+      <div class="casilla negro"></div>
+    </div>
+    <div class="filas">
+      <div class="casilla negro"></div>
+      <div class="casilla blanco"></div>
+      <div class="casilla negro"></div>
+      <div class="casilla blanco"></div>
+      <div class="casilla negro"></div>
+      <div class="casilla blanco"></div>
+      <div class="casilla negro"></div>
+      <div class="casilla blanco"></div>
+    </div>
+    <div class="filas">
+      <div class="casilla blanco"></div>
+      <div class="casilla negro"></div>
+      <div class="casilla blanco"></div>
+      <div class="casilla negro"></div>
+      <div class="casilla blanco"></div>
+      <div class="casilla negro"></div>
+      <div class="casilla blanco"></div>
+      <div class="casilla negro"></div>
+    </div>
+    <div class="filas">
+      <div class="casilla negro"></div>
+      <div class="casilla blanco"></div>
+      <div class="casilla negro"></div>
+      <div class="casilla blanco"></div>
+      <div class="casilla negro"></div>
+      <div class="casilla blanco"></div>
+      <div class="casilla negro"></div>
+      <div class="casilla blanco"></div>
+    </div>
+    <div class="filas">
+      <div class="casilla blanco"></div>
+      <div class="casilla negro"></div>
+      <div class="casilla blanco"></div>
+      <div class="casilla negro"></div>
+      <div class="casilla blanco"></div>
+      <div class="casilla negro"></div>
+      <div class="casilla blanco"></div>
+      <div class="casilla negro"></div>
+    </div>
+    <div class="filas">
+      <div class="casilla negro"></div>
+      <div class="casilla blanco"></div>
+      <div class="casilla negro"></div>
+      <div class="casilla blanco"></div>
+      <div class="casilla negro"></div>
+      <div class="casilla blanco"></div>
+      <div class="casilla negro"></div>
+      <div class="casilla blanco"></div>
+    </div>
+  </div>
+</main>
+
+<style>
+  .tablero{
+    background-color: rgb(131, 72, 0);
+    height: 200px;
+    width: 200;
+  }
+
+  .casilla{
+    height: 40px;
+    width: 40px;
+    display: flex;
+    flex-direction: row;
+  }
+
+  .blanco{
+    background-color: white;
+
+  }
+
+  .negro{
+    background-color: black;
+
+  }
+
+</style>
